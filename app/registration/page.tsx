5c7aa7c1d92bbb49d09a678fecc295bb711dc76a
asadpisatel <berdibaev.asadbek@gmail.com>
2025-02-02 15:54:01 +0500

    designed the registration page

--- app/registration/page.tsx
+++ app/registration/page.tsx
@@ -1,7 +1,129 @@
+"use client";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import React from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const minPassLen = 2;
+
+const schema = z
+  .object({
+    email: z.string().email(),
+    password: z.string().min(minPassLen),
+    passwordConfirm: z.string(),
+  })
+  .refine(
+    (data) => {
+      return data.password === data.passwordConfirm;
+    },
+    {
+      message: "Passwords do not match",
+      path: ["passwordConfirm"],
+    }
+  );
+
+type FormFields = z.infer<typeof schema>;
 
 const Page = () => {
-  return <div className="">Sign up</div>;
+  const form = useForm<FormFields>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      email: "",
+      password: "",
+      passwordConfirm: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof schema>) {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      console.log(values);
+    } catch (error) {
+      form.setError("email", {
+        message: "This email is already taken",
+      });
+      console.log(error);
+    }
+  }
+
+  return (
+    <div className="flex flex-col justify-center items-center gap-4 min-h-screen p-4">
+      <h2 className="text-3xl">Sign up</h2>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="max-w-md w-full space-y-4">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder="example@gmail.com"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="passwordConfirm"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password Confirm</FormLabel>
+                <FormControl>
+                  <Input type="password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button
+            disabled={form.formState.isSubmitting}
+            type="submit"
+            className="w-full">
+            {form.formState.isSubmitting ? "Loading..." : "Submit"}
+          </Button>
+        </form>
+      </Form>
+      <p>
+        Already have an account?{" "}
+        <Link href="/login" className="text-blue-700 underline">
+          Sign in
+        </Link>
+      </p>
+    </div>
+  );
 };
 
 export default Page;
